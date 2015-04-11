604aea25bc7a1b98fb91b41c9bc26c6ac71c89f8
Dake Sew <dakesew@yahoo.de>
2015-04-11 19:18:20 +0200

    Added comments to Servo example

--- TIM_SERVO/Src/main.c
+++ TIM_SERVO/Src/main.c
@@ -9,7 +9,7 @@ static void SystemClock_Config(void);
 
 uint32_t uhPrescalerValue = 0;
 
-#define  PERIOD_VALUE       (uint32_t)(22000)  /* Period Value  */
+#define  PERIOD_VALUE       (uint32_t)(22000)  /* Period Value, because we have need about 20 ms Pause between Pulses (20ms+2ms)in ns = 22000ns  */
 uint32_t CMP_VAL = 0;
 
 int main(void)
@@ -18,7 +18,7 @@ int main(void)
     HAL_Init();
     SystemClock_Config();
     /* -2- Calculate Prescaler */
-    //We want the Timer to run with 8MHz (the maximum, since we're running of the internal Clock, which runs at 8MHz, without PLL)
+    //We want the Timer to run with 1MHz, so we have a 1ns Timescale
     uhPrescalerValue = (uint32_t) (SystemCoreClock / 1000000) - 1;
 
     /* -3- Enable Clocks*/
@@ -44,7 +44,7 @@ int main(void)
     TIM_HandleStruct.Init.RepetitionCounter = 0;
     HAL_TIM_PWM_Init(&TIM_HandleStruct);
 
-    /* -6- Configure PWM-Output*/
+    /* -6- Configure Timer Output*/
     TIM_OC_InitStruct.OCMode = TIM_OCMODE_PWM1;
     TIM_OC_InitStruct.OCPolarity = TIM_OCPOLARITY_HIGH;
     TIM_OC_InitStruct.OCFastMode = TIM_OCFAST_DISABLE;
@@ -54,26 +54,24 @@ int main(void)
     TIM_OC_InitStruct.Pulse = CMP_VAL;
     HAL_TIM_PWM_ConfigChannel(&TIM_HandleStruct, &TIM_OC_InitStruct, TIM_CHANNEL_3);
 
-    /* -7- Enable Timer and PWM Output*/
+    /* -7- Enable Timer Output*/
     HAL_TIM_PWM_Start(&TIM_HandleStruct, TIM_CHANNEL_3);
 
     while (1) {
-        /* -8- Illuminate LED*/
+        /* -8- From lest to right*/
         for (CMP_VAL = 1000; CMP_VAL != 2000; CMP_VAL++) {
             TIM_OC_InitStruct.Pulse = CMP_VAL;
             HAL_TIM_PWM_ConfigChannel(&TIM_HandleStruct, &TIM_OC_InitStruct, TIM_CHANNEL_3);
             HAL_TIM_PWM_Start(&TIM_HandleStruct, TIM_CHANNEL_3);
             HAL_Delay(2);
         }
-        /* -8- Wait a bit at full brightness*/
-        /* -10- Dim LED*/
+        /* -9- From right to left*/
         for (CMP_VAL = 2000; CMP_VAL != 1000; CMP_VAL--) {
             TIM_OC_InitStruct.Pulse = CMP_VAL;
             HAL_TIM_PWM_ConfigChannel(&TIM_HandleStruct, &TIM_OC_InitStruct, TIM_CHANNEL_3);
             HAL_TIM_PWM_Start(&TIM_HandleStruct, TIM_CHANNEL_3);
             HAL_Delay(2);
         }
-        /* -11- Leave it dark*/
     }
 }
 
