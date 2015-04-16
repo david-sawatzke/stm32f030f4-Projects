752fe267fb3f21aa4d89113706ddd08f8a317ec5
Dake Sew <dakesew@yahoo.de>
2015-04-16 17:19:30 +0200

    Accidentally used the wrong folder

---
+++ HD44780_UART_GPIO/Src/main.c
@@ -0,0 +1,313 @@
+
+#include "main.h"
+
+#define DISP_CLR                 (1<<0)    // DB0: clear display
+#define DISP_RST                 (0x30)    // Reset Display
+#define DISP_HOME                (1<<1)    // DB1: return to home position
+
+#define DISP_ENTRY_MODE          (1<<2)    // DB2: set entry mode
+#define DISP_ENTRY_INC           (1<<1)    // DB1: 1=increment, 0=decrement
+#define DISP_ENTRY_SHIFT         (1<<0)    // DB0: 1=display shift on
+
+#define DISP_DISPLAYMODE         (1<<3)    // DB3: turn lcd/cursor on
+#define DISP_DISPLAYMODE_ON      (1<<2)    // DB2: turn display on
+#define DISP_DISPLAYMODE_CURSOR  (1<<1)    // DB1: turn cursor on
+#define DISP_DISPLAYMODE_BLINK   (1<<0)    // DB0: blinking cursor
+
+#define DISP_MOVE                (1<<4)    // DB4: move cursor/display
+#define DISP_MOVE_DISP           (1<<3)    // DB3: move display (0-> cursor)
+#define DISP_MOVE_RIGHT          (1<<2)    // DB2: move right (0-> left)
+
+#define DISP_FUNCTION            (1<<5)    // DB5: function set
+#define DISP_FUNCTION_8BIT       (1<<4)    // DB4: set 8BIT mode (0->4BIT mode)
+#define DISP_FUNCTION_2LINES     (1<<3)    // DB3: two lines (0->one line)
+#define DISP_FUNCTION_10DOTS     (1<<2)    // DB2: 5x10 font (0->5x7 font)
+
+#define DISP_CGRAM               (1<<6)    // DB6: set CG RAM address
+#define DISP_DDRAM               (1<<7)    // DB7: set DD RAM address
+
+#define DISP_BUSY                (1<<7)    // DB7: DISP is busy
+
+#define DISP_DDADR_LINE1         0x00
+#define DISP_DDADR_LINE2         0x40
+#define DISP_DDADR_LINE3         0x10
+#define DISP_DDADR_LINE4         0x50
+
+typedef struct {
+    GPIO_TypeDef *Port;
+    uint32_t Pin;
+} DISP_PinTypeDef;
+
+typedef struct {
+    DISP_PinTypeDef D4;
+    DISP_PinTypeDef D5;
+    DISP_PinTypeDef D6;
+    DISP_PinTypeDef D7;
+    DISP_PinTypeDef RS;
+    DISP_PinTypeDef E;
+    uint8_t Mode;
+    uint8_t Lines;
+} DISP_HandleTypeDef;
+DISP_HandleTypeDef DISP_HandleStruct;
+
+void DISP_GPIO_Clk_Init(DISP_HandleTypeDef *DISP_HandleStruct);
+HAL_StatusTypeDef DISP_Init(DISP_HandleTypeDef *DISP_HandleStruct);
+void DISP_Write(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t data, uint8_t rs);
+void DISP_CMD(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t cmd);
+void DISP_Putc(DISP_HandleTypeDef *DISP_HandleStruct, char c);
+void DISP_Out(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t data);
+void DISP_Setcursor(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t x, uint8_t y);
+void DISP_Puts(DISP_HandleTypeDef *DISP_HandleStruct, char *data);
+void DISP_PutInt(DISP_HandleTypeDef *DISP_HandleStruct, int pData);
+void DISP_Delay(uint16_t t);
+
+
+
+void SystemClock_Config(void);
+
+int main(void)
+{
+    int i = 0;
+    HAL_Init();
+    SystemClock_Config();
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+    HAL_Delay(500);
+    DISP_HandleStruct.D4.Port = GPIOA;
+    DISP_HandleStruct.D4.Pin = GPIO_PIN_5;
+    DISP_HandleStruct.D5.Port = GPIOA;
+    DISP_HandleStruct.D5.Pin = GPIO_PIN_6;
+    DISP_HandleStruct.D6.Port = GPIOA;
+    DISP_HandleStruct.D6.Pin = GPIO_PIN_7;
+    DISP_HandleStruct.D7.Port = GPIOB;
+    DISP_HandleStruct.D7.Pin = GPIO_PIN_1;
+    DISP_HandleStruct.RS.Port = GPIOA;
+    DISP_HandleStruct.RS.Pin = GPIO_PIN_9;
+    DISP_HandleStruct.E.Port = GPIOA;
+    DISP_HandleStruct.E.Pin = GPIO_PIN_10;
+    DISP_HandleStruct.Lines = 2;
+    DISP_Init(&DISP_HandleStruct);
+    DISP_Puts(&DISP_HandleStruct, "Hello World!");
+    DISP_Setcursor(&DISP_HandleStruct, 0, 2);
+    DISP_Puts(&DISP_HandleStruct, "Bye, cruel World!");
+    HAL_Delay(1000);
+    DISP_CMD(&DISP_HandleStruct, DISP_CLR);
+    HAL_Delay(1);
+    while (1) {
+        DISP_Setcursor(&DISP_HandleStruct, 0, 1);
+        DISP_PutInt(&DISP_HandleStruct, i);
+        i++;
+        HAL_Delay(10);
+    }
+}
+
+void DISP_PutInt(DISP_HandleTypeDef *DISP_HandleStruct, int pData)
+{
+    if (pData < 0) {
+        DISP_Putc(DISP_HandleStruct, '-');
+        pData *= -1;
+    } else {
+        DISP_Putc(DISP_HandleStruct, '+');
+    }
+    DISP_Putc(DISP_HandleStruct, '0' + (pData / 1000000000)); //billion
+    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 100000000) % 10)); //hundred million
+    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 10000000) % 10)); //ten million
+    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 1000000) % 10)); //million
+    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 100000) % 10)); //hundred thousand
+    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 10000) % 10)); //hundred thousand
+    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 1000) % 10)); //thousand
+    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 100) % 10)); //hundred
+    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 10) % 10)); //ten
+    DISP_Putc(DISP_HandleStruct, '0' + (pData % 10)); //one
+
+}
+
+HAL_StatusTypeDef DISP_Init(DISP_HandleTypeDef *DISP_HandleStruct)
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+    //Enable Clock for all Pins
+    DISP_GPIO_Clk_Init(DISP_HandleStruct);
+    //Set All Pins as Output
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    GPIO_InitStruct.Pin = DISP_HandleStruct->D4.Pin;
+    HAL_GPIO_Init(DISP_HandleStruct->D4.Port, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = DISP_HandleStruct->D5.Pin;
+    HAL_GPIO_Init(DISP_HandleStruct->D5.Port, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = DISP_HandleStruct->D6.Pin;
+    HAL_GPIO_Init(DISP_HandleStruct->D6.Port, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = DISP_HandleStruct->D7.Pin;
+    HAL_GPIO_Init(DISP_HandleStruct->D7.Port, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = DISP_HandleStruct->RS.Pin;
+    HAL_GPIO_Init(DISP_HandleStruct->RS.Port, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = DISP_HandleStruct->E.Pin;
+    HAL_GPIO_Init(DISP_HandleStruct->E.Port, &GPIO_InitStruct);
+    //Control Pins High
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(DISP_HandleStruct->RS.Port, DISP_HandleStruct->RS.Pin, GPIO_PIN_RESET);
+    //Data Lines Low
+    DISP_Out(DISP_HandleStruct, 0x00);
+    //Startup Delay
+    HAL_Delay(15);
+    //Init Display
+    //soft-reset
+    DISP_Out(DISP_HandleStruct, DISP_RST);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
+    HAL_Delay(1);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
+    HAL_Delay(5);
+
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
+    HAL_Delay(1);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
+    HAL_Delay(1);
+
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
+    HAL_Delay(1);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
+    HAL_Delay(1);
+
+    //4-bit Mode
+    DISP_Out(DISP_HandleStruct, DISP_FUNCTION);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
+    HAL_Delay(1);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
+    HAL_Delay(5);
+
+    if (DISP_HandleStruct->Lines < 1) {
+        DISP_CMD(DISP_HandleStruct, DISP_FUNCTION | DISP_FUNCTION_2LINES);
+    }
+
+    //Display On, CURSOR and Blink off
+    DISP_CMD(DISP_HandleStruct, DISP_DISPLAYMODE | DISP_DISPLAYMODE_ON);
+    //Entry Mode Set
+    DISP_CMD(DISP_HandleStruct, DISP_ENTRY_MODE | DISP_ENTRY_INC);
+    DISP_CMD(DISP_HandleStruct, DISP_CLR);
+    HAL_Delay(2);
+
+    return HAL_OK;
+}
+
+void DISP_GPIO_Clk_Init(DISP_HandleTypeDef *DISP_HandleStruct)
+{
+    if ((DISP_HandleStruct->D4.Port == GPIOA) || (DISP_HandleStruct->D5.Port == GPIOA) ||
+            (DISP_HandleStruct->D6.Port == GPIOA) || (DISP_HandleStruct->D7.Port == GPIOA) ||
+            (DISP_HandleStruct->RS.Port == GPIOA) || (DISP_HandleStruct->E.Port == GPIOA)) {
+        __GPIOA_CLK_ENABLE();
+    }
+    if ((DISP_HandleStruct->D4.Port == GPIOB) || (DISP_HandleStruct->D5.Port == GPIOB) ||
+            (DISP_HandleStruct->D6.Port == GPIOB) || (DISP_HandleStruct->D7.Port == GPIOB) ||
+            (DISP_HandleStruct->RS.Port == GPIOB) || (DISP_HandleStruct->E.Port == GPIOB)) {
+        __GPIOB_CLK_ENABLE();
+    }
+    if ((DISP_HandleStruct->D4.Port == GPIOC) || (DISP_HandleStruct->D5.Port == GPIOC) ||
+            (DISP_HandleStruct->D6.Port == GPIOC) || (DISP_HandleStruct->D7.Port == GPIOC) ||
+            (DISP_HandleStruct->RS.Port == GPIOC) || (DISP_HandleStruct->E.Port == GPIOC)) {
+        __GPIOC_CLK_ENABLE();
+    }
+#if defined(STM32F030x6) || defined(STM32F030x8) || \
+    defined(STM32F051x8) || defined(STM32F058xx) || defined(STM32F070xB) || \
+    defined(STM32F071xB) || defined(STM32F072xB) || defined(STM32F078xx) || \
+    defined(STM32F091xC) || defined(STM32F098xx) || defined(STM32F030xC)
+    if ((DISP_HandleStruct->D4.Port == GPIOD) || (DISP_HandleStruct->D5.Port == GPIOD) ||
+            (DISP_HandleStruct->D6.Port == GPIOD) || (DISP_HandleStruct->D7.Port == GPIOD) ||
+            (DISP_HandleStruct->RS.Port == GPIOD) || (DISP_HandleStruct->E.Port == GPIOD)) {
+        __GPIOD_CLK_ENABLE();
+    }
+#endif
+#if defined(STM32F071xB) || defined(STM32F072xB) || defined(STM32F078xx) || defined(STM32F070xB) || \
+    defined(STM32F091xC) || defined(STM32F098xx) || defined(STM32F030xC)
+    if ((DISP_HandleStruct->D4.Port == GPIOE) || (DISP_HandleStruct->D5.Port == GPIOE) ||
+            (DISP_HandleStruct->D6.Port == GPIOE) || (DISP_HandleStruct->D7.Port == GPIOE) ||
+            (DISP_HandleStruct->RS.Port == GPIOE) || (DISP_HandleStruct->E.Port == GPIOE)) {
+        __GPIOE_CLK_ENABLE();
+    }
+#endif
+    if ((DISP_HandleStruct->D4.Port == GPIOF) || (DISP_HandleStruct->D5.Port == GPIOF) ||
+            (DISP_HandleStruct->D6.Port == GPIOF) || (DISP_HandleStruct->D7.Port == GPIOF) ||
+            (DISP_HandleStruct->RS.Port == GPIOF) || (DISP_HandleStruct->E.Port == GPIOF)) {
+        __GPIOF_CLK_ENABLE();
+    }
+}
+
+void DISP_CMD(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t cmd)
+{
+    DISP_Write(DISP_HandleStruct, cmd, 0);
+}
+
+void DISP_Puts(DISP_HandleTypeDef *DISP_HandleStruct, char *data)
+{
+    while (*data != '\0') {
+        DISP_Putc(DISP_HandleStruct, *data++);
+    }
+}
+
+void DISP_Putc(DISP_HandleTypeDef *DISP_HandleStruct, char c)
+{
+    DISP_Write(DISP_HandleStruct, c, 1);
+}
+
+void DISP_Out(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t data)
+{
+    HAL_GPIO_WritePin(DISP_HandleStruct->D7.Port, DISP_HandleStruct->D7.Pin, data & (1 << 7));
+    HAL_GPIO_WritePin(DISP_HandleStruct->D6.Port, DISP_HandleStruct->D6.Pin, data & (1 << 6));
+    HAL_GPIO_WritePin(DISP_HandleStruct->D5.Port, DISP_HandleStruct->D5.Pin, data & (1 << 5));
+    HAL_GPIO_WritePin(DISP_HandleStruct->D4.Port, DISP_HandleStruct->D4.Pin, data & (1 << 4));
+}
+
+//WARNING: If you don't have 8MHz as your clock frequency, change the Delay values
+void DISP_Write(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t data, uint8_t rs)
+{
+    HAL_GPIO_WritePin(DISP_HandleStruct->RS.Port, DISP_HandleStruct->RS.Pin, rs);
+
+    DISP_Out(DISP_HandleStruct, data);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
+    DISP_Delay(50);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
+
+    DISP_Out(DISP_HandleStruct, (data << 4));
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
+    DISP_Delay(50);
+    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
+    DISP_Delay(20);
+
+    DISP_Out(DISP_HandleStruct, 0xff);
+}
+
+void DISP_Setcursor(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t x, uint8_t y)
+{
+    uint8_t data;
+
+    switch (y) {
+    case 1: // 1. Line
+        data = DISP_DDRAM + DISP_DDADR_LINE1 + x;
+        break;
+
+    case 2: // 2. Line
+        data = DISP_DDRAM + DISP_DDADR_LINE2 + x;
+        break;
+
+    case 3: // 3. Line
+        data = DISP_DDRAM + DISP_DDADR_LINE3 + x;
+        break;
+
+    case 4: // 4. Line
+        data = DISP_DDRAM + DISP_DDADR_LINE4 + x;
+        break;
+
+    default:
+        return;
+    }
+
+    DISP_CMD(DISP_HandleStruct, data);
+}
+
+void DISP_Delay(uint16_t t)
+{
+    volatile uint16_t i;
+    for (i = 0; i <= t; i++);
+}
+
+void SystemClock_Config(void)
+{
+
+}
--- HD44780_UART_Reciever/Src/main.c
+++ HD44780_UART_Reciever/Src/main.c
@@ -1,77 +1,19 @@
 
 #include "main.h"
 
-#define DISP_CLR                 (1<<0)    // DB0: clear display
-#define DISP_RST                 (0x30)    // Reset Display
-#define DISP_HOME                (1<<1)    // DB1: return to home position
-
-#define DISP_ENTRY_MODE          (1<<2)    // DB2: set entry mode
-#define DISP_ENTRY_INC           (1<<1)    // DB1: 1=increment, 0=decrement
-#define DISP_ENTRY_SHIFT         (1<<0)    // DB0: 1=display shift on
-
-#define DISP_DISPLAYMODE         (1<<3)    // DB3: turn lcd/cursor on
-#define DISP_DISPLAYMODE_ON      (1<<2)    // DB2: turn display on
-#define DISP_DISPLAYMODE_CURSOR  (1<<1)    // DB1: turn cursor on
-#define DISP_DISPLAYMODE_BLINK   (1<<0)    // DB0: blinking cursor
-
-#define DISP_MOVE                (1<<4)    // DB4: move cursor/display
-#define DISP_MOVE_DISP           (1<<3)    // DB3: move display (0-> cursor)
-#define DISP_MOVE_RIGHT          (1<<2)    // DB2: move right (0-> left)
-
-#define DISP_FUNCTION            (1<<5)    // DB5: function set
-#define DISP_FUNCTION_8BIT       (1<<4)    // DB4: set 8BIT mode (0->4BIT mode)
-#define DISP_FUNCTION_2LINES     (1<<3)    // DB3: two lines (0->one line)
-#define DISP_FUNCTION_10DOTS     (1<<2)    // DB2: 5x10 font (0->5x7 font)
-
-#define DISP_CGRAM               (1<<6)    // DB6: set CG RAM address
-#define DISP_DDRAM               (1<<7)    // DB7: set DD RAM address
-
-#define DISP_BUSY                (1<<7)    // DB7: DISP is busy
-
-#define DISP_DDADR_LINE1         0x00
-#define DISP_DDADR_LINE2         0x40
-#define DISP_DDADR_LINE3         0x10
-#define DISP_DDADR_LINE4         0x50
-
-typedef struct {
-    GPIO_TypeDef *Port;
-    uint32_t Pin;
-} DISP_PinTypeDef;
-
-typedef struct {
-    DISP_PinTypeDef D4;
-    DISP_PinTypeDef D5;
-    DISP_PinTypeDef D6;
-    DISP_PinTypeDef D7;
-    DISP_PinTypeDef RS;
-    DISP_PinTypeDef E;
-    uint8_t Mode;
-    uint8_t Lines;
-} DISP_HandleTypeDef;
+GPIO_InitTypeDef GPIO_InitStruct;
+UART_HandleTypeDef UartHandle;
 DISP_HandleTypeDef DISP_HandleStruct;
-
-void DISP_GPIO_Clk_Init(DISP_HandleTypeDef *DISP_HandleStruct);
-HAL_StatusTypeDef DISP_Init(DISP_HandleTypeDef *DISP_HandleStruct);
-void DISP_Write(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t data, uint8_t rs);
-void DISP_CMD(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t cmd);
-void DISP_Putc(DISP_HandleTypeDef *DISP_HandleStruct, char c);
-void DISP_Out(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t data);
-void DISP_Setcursor(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t x, uint8_t y);
-void DISP_Puts(DISP_HandleTypeDef *DISP_HandleStruct, char *data);
-void DISP_PutInt(DISP_HandleTypeDef *DISP_HandleStruct, int pData);
-void DISP_Delay(uint16_t t);
-
-
-
+char Msg [40];
+uint8_t PosX = 0;
+uint8_t PosY = 1;
 void SystemClock_Config(void);
 
 int main(void)
 {
-    int i = 0;
     HAL_Init();
     SystemClock_Config();
     HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
-    HAL_Delay(500);
     DISP_HandleStruct.D4.Port = GPIOA;
     DISP_HandleStruct.D4.Pin = GPIO_PIN_5;
     DISP_HandleStruct.D5.Port = GPIOA;
@@ -86,225 +28,83 @@ int main(void)
     DISP_HandleStruct.E.Pin = GPIO_PIN_10;
     DISP_HandleStruct.Lines = 2;
     DISP_Init(&DISP_HandleStruct);
-    DISP_Puts(&DISP_HandleStruct, "Hello World!");
-    DISP_Setcursor(&DISP_HandleStruct, 0, 2);
-    DISP_Puts(&DISP_HandleStruct, "Bye, cruel World!");
-    HAL_Delay(1000);
-    DISP_CMD(&DISP_HandleStruct, DISP_CLR);
-    HAL_Delay(1);
+    __GPIOA_CLK_ENABLE();
+    __USART1_CLK_ENABLE();
+
+    GPIO_InitStruct.Pin = GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF1_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    UartHandle.Instance = USART1;
+    UartHandle.Init.BaudRate = 9600;
+    UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+    UartHandle.Init.StopBits = UART_STOPBITS_1;
+    UartHandle.Init.Parity = UART_PARITY_NONE;
+    UartHandle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    UartHandle.Init.Mode = UART_MODE_RX;
+    UartHandle.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+    HAL_UART_Init(&UartHandle);
+
+    DISP_Puts(&DISP_HandleStruct, "UART Terminal");
+    HAL_UART_Receive(&UartHandle, (uint8_t *) & Msg[0], 1, 0xFFFFFFFF);
+    DISP_Setcursor(&DISP_HandleStruct, 0, PosY);
+    for (PosX = 0; PosX < 13; PosX++) {
+        DISP_Putc(&DISP_HandleStruct, ' ');
+    }
+    PosX = 0;
+    DISP_Setcursor(&DISP_HandleStruct, 0, PosY);
+    DISP_CMD(&DISP_HandleStruct, DISP_DISPLAYMODE | DISP_DISPLAYMODE_ON | DISP_DISPLAYMODE_BLINK);
     while (1) {
-        DISP_Setcursor(&DISP_HandleStruct, 0, 1);
-        DISP_PutInt(&DISP_HandleStruct, i);
-        i++;
-        HAL_Delay(10);
+        if (Msg[0] == '\r') {
+            if (PosY == 1) {
+                PosY = 2;
+            } else {
+                PosY = 1;
+            }
+            DISP_Setcursor(&DISP_HandleStruct, 0, PosY);
+            for (PosX = 0; PosX < 16; PosX++) {
+                DISP_Putc(&DISP_HandleStruct, ' ');
+            }
+            PosX = 0;
+            DISP_Setcursor(&DISP_HandleStruct, 0, PosY);
+        } else if ((Msg[0] == 127) || (Msg[0] == '\b')) {
+            if (PosX == 0) {
+                if (PosY == 1) {
+                    PosY = 2;
+                } else {
+                    PosY = 1;
+                }
+                PosX = 16;
+            }
+            DISP_Setcursor(&DISP_HandleStruct, PosX - 1, PosY);
+            DISP_Putc(&DISP_HandleStruct, ' ');
+            DISP_Setcursor(&DISP_HandleStruct, PosX - 1, PosY);
+            PosX--;
+        } else {
+            if (PosX >= 16) {
+                if (PosY == 1) {
+                    PosY = 2;
+                } else {
+                    PosY = 1;
+                }
+                DISP_Setcursor(&DISP_HandleStruct, 0, PosY);
+                for (PosX = 0; PosX < 16; PosX++) {
+                    DISP_Putc(&DISP_HandleStruct, ' ');
+                }
+                PosX = 0;
+                DISP_Setcursor(&DISP_HandleStruct, PosX, PosY);
+            }
+            DISP_Putc(&DISP_HandleStruct, Msg[0]);
+            PosX++;
+        }
+        HAL_UART_Receive(&UartHandle, (uint8_t *) & Msg[0], 1, 0xFFFFFFFF);
+
+
     }
-}
-
-void DISP_PutInt(DISP_HandleTypeDef *DISP_HandleStruct, int pData)
-{
-    if (pData < 0) {
-        DISP_Putc(DISP_HandleStruct, '-');
-        pData *= -1;
-    } else {
-        DISP_Putc(DISP_HandleStruct, '+');
-    }
-    DISP_Putc(DISP_HandleStruct, '0' + (pData / 1000000000)); //billion
-    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 100000000) % 10)); //hundred million
-    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 10000000) % 10)); //ten million
-    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 1000000) % 10)); //million
-    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 100000) % 10)); //hundred thousand
-    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 10000) % 10)); //hundred thousand
-    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 1000) % 10)); //thousand
-    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 100) % 10)); //hundred
-    DISP_Putc(DISP_HandleStruct, '0' + ((pData / 10) % 10)); //ten
-    DISP_Putc(DISP_HandleStruct, '0' + (pData % 10)); //one
-
-}
-
-HAL_StatusTypeDef DISP_Init(DISP_HandleTypeDef *DISP_HandleStruct)
-{
-    GPIO_InitTypeDef GPIO_InitStruct;
-    //Enable Clock for all Pins
-    DISP_GPIO_Clk_Init(DISP_HandleStruct);
-    //Set All Pins as Output
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-    GPIO_InitStruct.Pin = DISP_HandleStruct->D4.Pin;
-    HAL_GPIO_Init(DISP_HandleStruct->D4.Port, &GPIO_InitStruct);
-    GPIO_InitStruct.Pin = DISP_HandleStruct->D5.Pin;
-    HAL_GPIO_Init(DISP_HandleStruct->D5.Port, &GPIO_InitStruct);
-    GPIO_InitStruct.Pin = DISP_HandleStruct->D6.Pin;
-    HAL_GPIO_Init(DISP_HandleStruct->D6.Port, &GPIO_InitStruct);
-    GPIO_InitStruct.Pin = DISP_HandleStruct->D7.Pin;
-    HAL_GPIO_Init(DISP_HandleStruct->D7.Port, &GPIO_InitStruct);
-    GPIO_InitStruct.Pin = DISP_HandleStruct->RS.Pin;
-    HAL_GPIO_Init(DISP_HandleStruct->RS.Port, &GPIO_InitStruct);
-    GPIO_InitStruct.Pin = DISP_HandleStruct->E.Pin;
-    HAL_GPIO_Init(DISP_HandleStruct->E.Port, &GPIO_InitStruct);
-    //Control Pins High
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(DISP_HandleStruct->RS.Port, DISP_HandleStruct->RS.Pin, GPIO_PIN_RESET);
-    //Data Lines Low
-    DISP_Out(DISP_HandleStruct, 0x00);
-    //Startup Delay
-    HAL_Delay(15);
-    //Init Display
-    //soft-reset
-    DISP_Out(DISP_HandleStruct, DISP_RST);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
-    HAL_Delay(1);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
-    HAL_Delay(5);
-
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
-    HAL_Delay(1);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
-    HAL_Delay(1);
-
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
-    HAL_Delay(1);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
-    HAL_Delay(1);
-
-    //4-bit Mode
-    DISP_Out(DISP_HandleStruct, DISP_FUNCTION);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
-    HAL_Delay(1);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
-    HAL_Delay(5);
-
-    if (DISP_HandleStruct->Lines < 1) {
-        DISP_CMD(DISP_HandleStruct, DISP_FUNCTION | DISP_FUNCTION_2LINES);
-    }
-
-    //Display On, CURSOR and Blink off
-    DISP_CMD(DISP_HandleStruct, DISP_DISPLAYMODE | DISP_DISPLAYMODE_ON);
-    //Entry Mode Set
-    DISP_CMD(DISP_HandleStruct, DISP_ENTRY_MODE | DISP_ENTRY_INC);
-    DISP_CMD(DISP_HandleStruct, DISP_CLR);
-    HAL_Delay(2);
-
-    return HAL_OK;
-}
-
-void DISP_GPIO_Clk_Init(DISP_HandleTypeDef *DISP_HandleStruct)
-{
-    if ((DISP_HandleStruct->D4.Port == GPIOA) || (DISP_HandleStruct->D5.Port == GPIOA) ||
-            (DISP_HandleStruct->D6.Port == GPIOA) || (DISP_HandleStruct->D7.Port == GPIOA) ||
-            (DISP_HandleStruct->RS.Port == GPIOA) || (DISP_HandleStruct->E.Port == GPIOA)) {
-        __GPIOA_CLK_ENABLE();
-    }
-    if ((DISP_HandleStruct->D4.Port == GPIOB) || (DISP_HandleStruct->D5.Port == GPIOB) ||
-            (DISP_HandleStruct->D6.Port == GPIOB) || (DISP_HandleStruct->D7.Port == GPIOB) ||
-            (DISP_HandleStruct->RS.Port == GPIOB) || (DISP_HandleStruct->E.Port == GPIOB)) {
-        __GPIOB_CLK_ENABLE();
-    }
-    if ((DISP_HandleStruct->D4.Port == GPIOC) || (DISP_HandleStruct->D5.Port == GPIOC) ||
-            (DISP_HandleStruct->D6.Port == GPIOC) || (DISP_HandleStruct->D7.Port == GPIOC) ||
-            (DISP_HandleStruct->RS.Port == GPIOC) || (DISP_HandleStruct->E.Port == GPIOC)) {
-        __GPIOC_CLK_ENABLE();
-    }
-#if defined(STM32F030x6) || defined(STM32F030x8) || \
-    defined(STM32F051x8) || defined(STM32F058xx) || defined(STM32F070xB) || \
-    defined(STM32F071xB) || defined(STM32F072xB) || defined(STM32F078xx) || \
-    defined(STM32F091xC) || defined(STM32F098xx) || defined(STM32F030xC)
-    if ((DISP_HandleStruct->D4.Port == GPIOD) || (DISP_HandleStruct->D5.Port == GPIOD) ||
-            (DISP_HandleStruct->D6.Port == GPIOD) || (DISP_HandleStruct->D7.Port == GPIOD) ||
-            (DISP_HandleStruct->RS.Port == GPIOD) || (DISP_HandleStruct->E.Port == GPIOD)) {
-        __GPIOD_CLK_ENABLE();
-    }
-#endif
-#if defined(STM32F071xB) || defined(STM32F072xB) || defined(STM32F078xx) || defined(STM32F070xB) || \
-    defined(STM32F091xC) || defined(STM32F098xx) || defined(STM32F030xC)
-    if ((DISP_HandleStruct->D4.Port == GPIOE) || (DISP_HandleStruct->D5.Port == GPIOE) ||
-            (DISP_HandleStruct->D6.Port == GPIOE) || (DISP_HandleStruct->D7.Port == GPIOE) ||
-            (DISP_HandleStruct->RS.Port == GPIOE) || (DISP_HandleStruct->E.Port == GPIOE)) {
-        __GPIOE_CLK_ENABLE();
-    }
-#endif
-    if ((DISP_HandleStruct->D4.Port == GPIOF) || (DISP_HandleStruct->D5.Port == GPIOF) ||
-            (DISP_HandleStruct->D6.Port == GPIOF) || (DISP_HandleStruct->D7.Port == GPIOF) ||
-            (DISP_HandleStruct->RS.Port == GPIOF) || (DISP_HandleStruct->E.Port == GPIOF)) {
-        __GPIOF_CLK_ENABLE();
-    }
-}
-
-void DISP_CMD(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t cmd)
-{
-    DISP_Write(DISP_HandleStruct, cmd, 0);
-}
-
-void DISP_Puts(DISP_HandleTypeDef *DISP_HandleStruct, char *data)
-{
-    while (*data != '\0') {
-        DISP_Putc(DISP_HandleStruct, *data++);
-    }
-}
-
-void DISP_Putc(DISP_HandleTypeDef *DISP_HandleStruct, char c)
-{
-    DISP_Write(DISP_HandleStruct, c, 1);
-}
-
-void DISP_Out(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t data)
-{
-    HAL_GPIO_WritePin(DISP_HandleStruct->D7.Port, DISP_HandleStruct->D7.Pin, data & (1 << 7));
-    HAL_GPIO_WritePin(DISP_HandleStruct->D6.Port, DISP_HandleStruct->D6.Pin, data & (1 << 6));
-    HAL_GPIO_WritePin(DISP_HandleStruct->D5.Port, DISP_HandleStruct->D5.Pin, data & (1 << 5));
-    HAL_GPIO_WritePin(DISP_HandleStruct->D4.Port, DISP_HandleStruct->D4.Pin, data & (1 << 4));
-}
-
-//WARNING: If you don't have 8MHz as your clock frequency, change the Delay values
-void DISP_Write(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t data, uint8_t rs)
-{
-    HAL_GPIO_WritePin(DISP_HandleStruct->RS.Port, DISP_HandleStruct->RS.Pin, rs);
-
-    DISP_Out(DISP_HandleStruct, data);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
-    DISP_Delay(50);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
-
-    DISP_Out(DISP_HandleStruct, (data << 4));
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_SET);
-    DISP_Delay(50);
-    HAL_GPIO_WritePin(DISP_HandleStruct->E.Port, DISP_HandleStruct->E.Pin, GPIO_PIN_RESET);
-    DISP_Delay(20);
-
-    DISP_Out(DISP_HandleStruct, 0xff);
-}
-
-void DISP_Setcursor(DISP_HandleTypeDef *DISP_HandleStruct, uint8_t x, uint8_t y)
-{
-    uint8_t data;
-
-    switch (y) {
-    case 1: // 1. Line
-        data = DISP_DDRAM + DISP_DDADR_LINE1 + x;
-        break;
-
-    case 2: // 2. Line
-        data = DISP_DDRAM + DISP_DDADR_LINE2 + x;
-        break;
-
-    case 3: // 3. Line
-        data = DISP_DDRAM + DISP_DDADR_LINE3 + x;
-        break;
-
-    case 4: // 4. Line
-        data = DISP_DDRAM + DISP_DDADR_LINE4 + x;
-        break;
-
-    default:
-        return;
-    }
-
-    DISP_CMD(DISP_HandleStruct, data);
-}
-
-void DISP_Delay(uint16_t t)
-{
-    volatile uint16_t i;
-    for (i = 0; i <= t; i++);
+    return 0;
 }
 
 void SystemClock_Config(void)
